fc3152da35006dc23510cedf4aa89aa3d06ddf0d
wili rahmat muhamad <wilirahmatm@gmail.com>
2020-08-17 06:54:57 +0800

    update index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+$('.banner-slider').owlCarousel({
+      loop:true,
+      nav:false,
+      dots: false,
+      items:1,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      margin: 0,
+      mouseDrag: false,
+      touchDrag: false,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+      // animateOut: 'slideOutUp',
+      // animateIn: 'slideInDown',
+});
+
 $('.top-destination-slider').owlCarousel({
       loop:true,
       nav:false,
@@ -12,12 +28,13 @@ $('.top-destination-slider').owlCarousel({
       // animateOut: 'slideOutUp',
       // animateIn: 'slideInDown',
 });
-var owlE = $('.top-destination-slider');
+
+var owlTopDest = $('.top-destination-slider');
 owlE.owlCarousel();
     
 
 $('.top-destination-next').on('click', function() {
-    owlE.trigger('next.owl.carousel');
+    owlTopDest.trigger('next.owl.carousel');
 })
 
 $('.top-destination-prev').on('click', function () {
